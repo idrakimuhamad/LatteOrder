e25bbdca0d1dbc3d6d18489c4e48512116525d77
Idraki Muhamad <idrakimuhamad@gmail.com>
2015-01-14 22:50:09 +0800

    Added Closest Restaurant Locater

--- api/controllers/LocationsController.js
+++ api/controllers/LocationsController.js
@@ -6,6 +6,31 @@
  */
 
 module.exports = {
+  getClosestRestaurant: function (req, res) {
+    var lat = parseFloat(req.param('lat'));
+    var lng = parseFloat(req.param('lng'));
+    // if radius was passed, use it. else defaulted to 5
+    var radii = req.param('radii') ? parseFloat(req.param('radii')) : 5;
+
+    // using the lat and lng, uses Mongo's spatial
+    Locations.native(function(err,collection) {
+      if (err) return res.serverError(err);
+
+      var loc = collection.find({
+        'geometry.location': {
+          $geoWithin: {
+            $center: [ [ lat, lng ] , radii ]
+          }
+        }
+      }).toArray(function (err, results) {
+        if (err) return res.serverError(err);
+        return res.json({
+          restaurant: results
+        });
+      });
+
+    });
+  },
   clearAllLocations: function (req, res) {
     Locations.destroy({}).exec(function clearAllLocations(err) {
       console.log('All of the location has been deleted');

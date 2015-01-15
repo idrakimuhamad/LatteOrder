647a875dd235c31ac965631af90b86337cb0479a
Idraki Muhamad <idrakimuhamad@gmail.com>
2015-01-15 23:49:40 +0800

    added getOrderByOrderNumber, updateOrder
    standardise the callback

--- api/services/OrdersService.js
+++ api/services/OrdersService.js
@@ -6,27 +6,41 @@
  */
 
  module.exports = {
+  // check for current order number and rewrite it after increment
   orderNumber: function (callback) {
     OrderNumber.find().exec(function (err, od) {
       if (!err && od.length > 0) {
         var orderNumber = od[0].current + 1;
         OrderNumber.update({ id: od[0].id }, { current: orderNumber }).exec(function (err, update) {
-          if (!err) {
-            callback(null, 'SB' + orderNumber);
-          } else {
-            callback(err);
-          }
+          if (err) callback(err);
+          else callback(null, 'SB' + orderNumber);
         });
       } else {
         var defaultOD = 1000;
         OrderNumber.create({ current: defaultOD}).exec(function (err, update) {
-          if (!err) {
-            callback(null, 'SB' + defaultOD);
-          } else {
-            callback(err);
-          }
+          if (err) callback(err);
+          else callback(null, 'SB' + defaultOD);
         });
       }
     });
+  },
+  // lookup order by order number
+  getOrderByOrderNumber: function (order, callback) {
+    Orders.findOne({ orderNumber: order })
+            .exec(function (err, order) {
+              if (err) callback(err);
+              else callback(null, order);
+            })
+  },
+  // update order by any properties and value
+  updateOrder: function (order, toUpdate, callback) {
+    // toUpdate is an object that should be setup before calling this function
+    // it can be any of the property that an Order has
+    // eg: to change the status, the object should be pass as:
+    // { status: 'paid' }
+    Orders.update({ orderNumber: order}, toUpdate).exec(function (err, update) {
+      if (err) callback(err);
+      else callback(null, update);
+    })
   }
  };
